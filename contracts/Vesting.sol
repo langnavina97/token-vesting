fe3922101b469ff1afbea09012eb8e74d7b7003c
langnavina97 <81016364+langnavina97@users.noreply.github.com>
2022-02-05 23:39:42 -0500

    working version of backend

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -8,12 +8,13 @@ import "./XYZToken.sol";
 contract Vesting is Ownable, XYZToken {
     using SafeMath for uint256;
 
-    mapping(address => uint256) public shares;
     address[] beneficiaries;
-    uint256 beneficiaryCount;
-    uint256 releaseTime;
-    uint256 public endTime;
-    uint256 releaseCount;
+    // mapping to check whether the beneficary already exists (cheaper than a loop for the array)
+    mapping(address => bool) isBeneficiary;
+
+    uint256 startTime;
+    uint256 endTime;
+    uint256 lastRelease;
 
     XYZToken public token;
 
@@ -21,17 +22,16 @@ contract Vesting is Ownable, XYZToken {
     uint256 releaseAmount;
     uint256 tokensPerBeneficiaryPerMinute;
 
-    //uint256 public timeDiff;
-
     constructor() {
         // dispersed tokens per minute in total
         // 365 days * 24 hours * 60 minutes
         releases = 525600;
-        releaseAmount = 100000000 / releases;
+        releaseAmount = totalSupply() / releases;
         tokensPerBeneficiaryPerMinute = releaseAmount / 10;
 
-        releaseTime = block.timestamp;
-        endTime = releaseTime + 365 days;
+        startTime = block.timestamp;
+        endTime = startTime + 365 days;
+        lastRelease = startTime;
     }
 
     function addBeneficiaries(address _beneficiary) public onlyOwner {
@@ -40,33 +40,48 @@ contract Vesting is Ownable, XYZToken {
             "The address can't be a zero address"
         );
         require(
-            beneficiaryCount < 10 && shares[_beneficiary] == 0,
+            beneficiaries.length < 10,
             "There are already 10 beneficiaries."
         );
+        require(!isBeneficiary[_beneficiary]);
         // 10% share for each
-        shares[_beneficiary] = 10;
         beneficiaries.push(_beneficiary);
-        beneficiaryCount.add(1);
-    }
-
-    function calcTimeDifference() public view returns (uint256) {
-        uint256 timeDifference = block.timestamp.sub(releaseTime);
-
-        return timeDifference.div(60);
+        isBeneficiary[_beneficiary] = true;
     }
 
     function vest() public {
+        uint256 timeDifference;
+        uint256 minutesSinceLastRelease;
+        uint256 amount;
+
+        require(lastRelease < endTime);
+        // in case the year is already over and the tokens haven't been transferred in time
+        if (block.timestamp > endTime) {
+            timeDifference = endTime.sub(lastRelease);
+        } else {
+            timeDifference = block.timestamp.sub(lastRelease);
+        }
+        require(
+            timeDifference >= 60,
+            "The last release was less than one minute ago."
+        );
+
+        minutesSinceLastRelease = timeDifference.div(60);
+        amount = minutesSinceLastRelease.mul(tokensPerBeneficiaryPerMinute);
+
+        require(block.timestamp <= endTime || lastRelease < endTime);
+
         for (uint256 i = 0; i < beneficiaries.length; i++) {
-            release(beneficiaries[i]);
+            release(beneficiaries[i], amount);
         }
     }
 
-    function release(address _beneficiary) public payable virtual {
-        // in case the year is already over and the tokens haven't been transferred in time
-        uint256 timeDifference = block.timestamp.sub(releaseTime);
-        require(timeDifference >= 60);
-
-        transfer(payable(_beneficiary), tokensPerBeneficiaryPerMinute);
-        releaseCount++;
+    function release(address _beneficiary, uint256 amount)
+        public
+        payable
+        virtual
+    {
+        transfer(payable(_beneficiary), amount);
+        lastRelease = block.timestamp;
     }
 }
